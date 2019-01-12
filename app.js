710fb06933b4956c7fe63abaf455e1e3b54f3a03
Gonzalo Sobrino <gonzalosobrino6@gmail.com>
2019-01-12 14:29:13 +0100

    First commit

--- app.js
+++ app.js
@@ -17,4 +17,4 @@ app.set('view engine', 'hbs');
 hbs.registerPartials(__dirname + '/views/partials')
 app.use(express.static(__dirname + '/public'));
 
-app.listen(PORT, () => console.info(`App listen at ${PORT} port`));
+app.listen(PORT, () => console.info(`My app listen at ${PORT} port`));
