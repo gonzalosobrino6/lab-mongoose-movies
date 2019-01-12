5d341c1307363076f3d644528a556eb64634fcec
Gonzalo Sobrino <gonzalosobrino6@gmail.com>
2019-01-12 15:16:49 +0100

    rutas creadas

--- controllers/celebrity.controller.js
+++ controllers/celebrity.controller.js
@@ -1,1 +1,29 @@
-const celebrity = require('../models/celebrity.model')
+const Celebrity = require('../models/celebrity.model');
+
+module.exports.list = (req, res, next) => {
+  Celebrity.find()
+    .then((celebrities) => res.render('celebrities/list', { celebrities }))
+    .catch(err => next(err))
+}
+
+module.exports.create = (req, res, next) => {
+  res.render('celebrities/create');
+}
+
+module.exports.doCreate = (req, res, next) => {
+  const celebrity = new Celebrity(req.body);
+
+  celebrity.save()
+    .then((celebrity) => { res.redirect('/celebrities' )});
+}
+
+module.exports.get = (req, res, next) => {
+  Celebrity.findById(req.params.id)
+    .then(celebrity => res.render('celebrities/detail', { celebrity }));
+}
+
+module.exports.delete = (req, res, next) => {
+  Celebrity.findByIdAndDelete(req.params.id)
+    .then(celebrity => res.redirect('/celebrities'));
+}
+
